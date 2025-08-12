9fb76d62c6788c14ee29dbf5aa4d3e3e2c06ef04
milekboy <tijaniwahab25@gmail.com>
2025-08-12 10:47:35 +0100

    yes

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -36,7 +36,7 @@ export default function HeroCarousel({
 
   return (
     <section
-      className={`relative w-full ${heightClass} overflow-hidden`}
+      className={`relative lg:-mt-[83px] w-full ${heightClass} overflow-hidden`}
       onMouseEnter={pause}
       onMouseLeave={resume}
     >
